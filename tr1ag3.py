8432590f2a2f0d1c643259bce7e433601759bfd8
0sx86 <x86@0sx86.re>
2024-04-12 14:31:16 +0200

    v1

--- tr1ag3.py
+++ tr1ag3.py
@@ -2,19 +2,25 @@ import subprocess
 import os
 import sys
 import re
+import argparse
 
-def get_volatility_version():
-    # Execute vol.py command to get Volatility version
-    result = subprocess.run("vol.py 2>&1", shell=True, capture_output=True, text=True)
-    volatility_match = re.search(r"Volatility", result.stdout)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', help='the version of volatility', required=True)
+    parser.add_argument('-f', '--file', help='the file to analyse', required=True)
 
-    if volatility_match:
-        return 2  # Assuming Volatility 2.x
-    else:
-        return 3  # Assuming Volatility 3.x
+    return parser.parse_args()
 
-def find_profile(dump_file, major_version):
-    if major_version == 2:
+class Triage():
+    def __init__(self, ver=3):
+        
+        if ver == 3:
+            self.commands = ["windows.cmdline", "windows.pslist", "windows.netscan", "windows.filescan"]
+        else:
+            self.commands = ["consoles", "iehistory", "pslist", "netscan", "filescan"]
+        self.ver = ver
+
+    def find_profile(self, dump_file):
         # Execute vol.py command to get profile (only for Volatility 2.x)
         command = f"vol.py -f {dump_file} imageinfo"
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
@@ -22,64 +28,47 @@ def find_profile(dump_file, major_version):
         # Extract the profiles
         profile_match = re.search(r"Profile\(s\) : (.+)", result.stdout)
         
-        if profile_match:
-            return profile_match.group(1).strip().split(",")[0]  # Get the first profile
+        return profile_match if profile_match == None else profile_match.group(1).strip().split(",")[0]
+    
+    def run_command(self, dump_file, profile, command, output_folder):
+        # Determine command format based on Volatility version
+        if self.ver == 2:
+            command_format = f"vol.py -f {dump_file} --profile={profile} {command} 2>&1"
         else:
-            return None
+            command_format = f"vol -f {dump_file} {command} 2>&1"
+
+        # Execute Volatility command
+        output_file = os.path.join(output_folder, f"{command}_output.txt")
+        with open(output_file, 'w') as f:
+            subprocess.run(command_format, shell=True, stdout=f, stderr=subprocess.PIPE)
         
-    return None
+    def init(self, dump_file):
+        output_folder = "tr1ag3-output"
 
-def run_volatility_command(dump_file, profile, command, output_folder, major_version):
-    if major_version is None:
-        print("Error: Volatility not found.")
-        sys.exit(1)
-
-    # Determine command format based on Volatility version
-    if major_version == 2:
-        command_format = f"vol.py -f {dump_file} --profile={profile} {command}"
-    else:
-        command_format = f"vol.py -f {dump_file} {command}"
-
-    # Execute Volatility command
-    output_file = os.path.join(output_folder, f"{command}_output.txt")
-    with open(output_file, 'w') as f:
-        subprocess.run(command_format, shell=True, stdout=f, stderr=subprocess.PIPE)
-
-def main():
-    # Check if a dump file is provided as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: python3 tr1ag3.py <dump_file>")
-        sys.exit(1)
-
-    dump_file = sys.argv[1]
-    output_folder = "tr1ag3-output"
-
-    # Find the Volatility version
-    major_version = get_volatility_version()
-
-    # Check Volatility version and get profile if needed
-    if major_version is not None:
-        profile = find_profile(dump_file, major_version)
-        if profile is not None:
-            print(f"Profile found: {profile}")
-
-            # Create output folder if it doesn't exist
-            os.makedirs(output_folder, exist_ok=True)
-
-            # Define Volatility commands to run
-            if major_version == 2:
-                volatility_commands = ["consoles", "iehistory", "pslist", "netscan", "filescan"]
+        # Create output folder if it doesn't exist
+        os.makedirs(output_folder, exist_ok=True)
+        
+        profile = None
+        if self.ver == 2:
+            # Check Volatility version and get profile if needed
+            profile = self.find_profile(dump_file, self.ver)
+            if profile is not None:
+                print(f"Profile found: {profile}")
             else:
-                volatility_commands = ["windows.cmdline", "windows.pslist", "windows.netscan", "windows.filescan"]
+                print("Profile not found. Unable to proceed.")
+                return
+        
+        # Run each Volatility command and save output to a text file in the appropriate folder
+        for command in self.commands:
+            self.run_command(dump_file, profile, command, output_folder)
+            print(f"{command} command executed. Results saved to {output_folder}/{command}_output.txt")
 
-            # Run each Volatility command and save output to a text file in the appropriate folder
-            for command in volatility_commands:
-                run_volatility_command(dump_file, profile, command, output_folder, major_version)
-                print(f"{command} command executed. Results saved to {output_folder}/{command}_output.txt")
-        else:
-            print("Profile not found. Unable to proceed.")
-    else:
-        print("Error: Volatility not found.")
+
+def main(major_version, dump_file):
+    worker = Triage(major_version)
+    worker.init(dump_file)
 
 if __name__ == "__main__":
-    main()
+    opts = get_args()
+
+    sys.exit(main(opts.version, opts.file))
